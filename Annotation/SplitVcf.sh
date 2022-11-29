a74e25609160682c50f71973bfcb176e12b737a0
Jeffleecy <80674585+Jeffleecy@users.noreply.github.com>
2022-11-29 10:40:01 +0800

    Delete Annotation directory

--- Annotation/SplitVcf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=/pkg/biology/BCFtools/BCFtools_v1.10.2/bin:$PATH
-path=/work1/u1272905/1012DeafnessVariantCalling/JointCalling1012Samples/Split_954/JointCalling954.output-hc.vcf.gz
-file=/work1/u1272905/1012DeafnessVariantCalling/JointCalling1012Samples/Split_954/file2
-bcftools view -S $file --force-samples -Oz -o WS_subset.vcf.gz $path
-
-
